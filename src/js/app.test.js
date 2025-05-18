a526661c6be3335083fdc2b2c7dcde72350508c6
Tuan Anh <tp.anhh7@gmail.com>
2025-05-18 14:18:57 +0300

    add game function

--- src/js/app.test.js
+++ src/js/app.test.js
@@ -3,16 +3,31 @@ import Game from './game';
 describe('Game', () => {
   let game;
 
+  beforeAll(() => {
+    // Создаем минимальный DOM для тестов
+    document.body.innerHTML = `
+      <div class="game-container">
+        <div class="game-board" id="gameBoard"></div>
+      </div>
+    `;
+  });
+
   beforeEach(() => {
     game = new Game();
   });
 
   test('should initialize', () => {
     expect(game).toBeInstanceOf(Game);
+    expect(game.field).toBeDefined();
+    expect(game.score).toBeDefined();
+    expect(game.goblin).toBeDefined();
   });
 
   test('should start game', () => {
     game.start();
     expect(game.gameInterval).toBeDefined();
+
+    // Очищаем интервал после теста
+    clearInterval(game.gameInterval);
   });
 });
